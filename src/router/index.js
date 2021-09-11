2521f5f4dad0e419ee305ff19d1ffaf224ef2fc9
zhenyu.wang <wangzhenyu@e-tianrong.com>
2021-09-11 22:34:39 +0800

    feat:登录页面创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">Home</div>
 </template>
-
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   components: {},
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  color: red;
+}
+</style>
