7f1ccb40d54661f7a687db909ec1c7fba03ebcb9
zhenyuWang <13641039885@163.com>
2023-03-16 17:02:56 +0800

    fix(avatar): update avatar address

--- mock/user.js
+++ mock/user.js
@@ -9,7 +9,7 @@ Mock.mock('/api/user/login', 'post', (req) => {
     body: {
       userId: '001',
       name: data.name,
-      avatar: 'https://img0.baidu.com/it/u=3233551726,336273710&fm=26&fmt=auto&gp=0.jpg',
+      avatar: 'http://114.115.235.59/avatar.png',
     },
   }
 })
