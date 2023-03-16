0f481a7f6a2263abd21c73d1165e6835699faa73
zhenyuWang <13641039885@163.com>
2023-03-16 17:12:53 +0800

    chore(verify-commit-msg): add commit type

--- script/verify-commit-msg.js
+++ script/verify-commit-msg.js
@@ -4,7 +4,7 @@ const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim()
 
-const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build|version)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
   console.log()
